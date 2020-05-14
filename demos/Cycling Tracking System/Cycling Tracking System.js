30642d042fe0af7e60db90829aabe9aea03592dc
XieZuo7 <65163045+XieZuo7@users.noreply.github.com>
2020-05-14 19:14:22 +0800

    Update Cycling Tracking System.js

--- demos/Cycling Tracking System/Cycling Tracking System.js
+++ demos/Cycling Tracking System/Cycling Tracking System.js
@@ -17,7 +17,7 @@
         port.onReceive = data => {
           
           let textDecoder = new TextDecoder();
-          console.log(textDecoder.decode(data));
+          console.log(textDecoder.decode(data)+'1');
           console.log(5);
         }
         
