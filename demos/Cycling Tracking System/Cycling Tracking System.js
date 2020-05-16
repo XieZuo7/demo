ad62f109bc4179c35349948727b23da8e1758f58
XieZuo7 <65163045+XieZuo7@users.noreply.github.com>
2020-05-16 17:56:15 +0800

    Update Cycling Tracking System.js

--- demos/Cycling Tracking System/Cycling Tracking System.js
+++ demos/Cycling Tracking System/Cycling Tracking System.js
@@ -16,7 +16,7 @@
           let textDecoder = new TextDecoder();
           console.log(textDecoder.decode(data));
           //document.getElementById("value10").innerHTML = textDecoder.decode(data);
-          let view = new Array(3);
+          let view = new Array(5);
           var string = textDecoder.decode(data);
           var reg = /\d+/g;
           var number = string.match(reg);
@@ -29,7 +29,12 @@
           document.getElementById("valueL").innerHTML = view[1];
           document.getElementById("valueR").innerHTML = view[2];
           
-          
+          if(number[1] > 1000 && view[5] <1)
+		       {
+             view[4] = number[1];
+             view[5] = 1;
+             document.getElementById('value10').innerHTML++;
+           }else if(number[1] > 1000){view[5] = 0;}
           
           
         }
